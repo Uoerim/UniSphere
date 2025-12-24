aa95dcadb19d4b48789cdc83b5e4afc9b9a2cfa3
Yosif Ibrahim <yosufmaib@gmail.com>
2025-12-24 13:40:19 +0200

    Fixed Hotreload with docker

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import styles from './Dashboard.module.css';
 
 export default function Dashboard() {
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { Navigate } from "react-router-dom";
 import styles from "./Login.module.css";
 
